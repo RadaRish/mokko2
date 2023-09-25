b0fc2881e2f6f9579a56a81f4dc93edc58d03db4
RadaRish <107124341+RadaRish@users.noreply.github.com>
2023-09-25 15:31:42 +0300

    mokko2

---
+++ data.js
@@ -0,0 +1,533 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-------",
+      "name": "Пер. М. Казем-Бека (улица Счастливых людей)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.603007761248282,
+        "pitch": 0.02406988377698127,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4481177773643559,
+          "pitch": 0.15078393805164225,
+          "rotation": 6.283185307179586,
+          "target": "1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "Вход в Мокко",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.7990086435603798,
+        "pitch": 0.03567208144804468,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.090313769983357,
+          "pitch": 0.08657301515925298,
+          "rotation": 0,
+          "target": "0-------"
+        },
+        {
+          "yaw": 2.0375518561639048,
+          "pitch": 0.09278693640110092,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Зал, десерты",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.7504915783575683,
+        "pitch": 0.07599584802809822,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.163164257757824,
+          "pitch": 0.4880696779163447,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -1.1648547269058085,
+          "pitch": 0.2538627182517139,
+          "rotation": 0.7853981633974483,
+          "target": "5---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Зал, бар",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.29282746339016086,
+        "pitch": 0.04559750881686497,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4217559812447682,
+          "pitch": 0.2460884805714887,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 2.986783271634624,
+          "pitch": 0.2335254904623021,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Зал, стол",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.2262675435618258,
+        "pitch": 0.580101639947948,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.130753512907596,
+          "pitch": 0.24632414938518465,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5---",
+      "name": "Лестница на террасу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5220573755271793,
+        "pitch": -0.2580159273243794,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5251796354367642,
+          "pitch": 0.17656439591504736,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": 3.073929741565225,
+          "pitch": 0.4149083339048225,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 0.0450797954721569,
+          "pitch": -0.11572076847480162,
+          "rotation": 0.7853981633974483,
+          "target": "8----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "Санузел, раковина",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3614369616986153,
+          "pitch": 0.19755249854097912,
+          "rotation": 0,
+          "target": "5---"
+        },
+        {
+          "yaw": 0.9299037743052487,
+          "pitch": 0.20818988033472863,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "Санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37129013404059563,
+          "pitch": 0.3497522201335652,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8----",
+      "name": " Лестница на террасу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.763011487189635,
+        "pitch": -0.048130703751143145,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.584362913786215,
+          "pitch": 0.7797567206387459,
+          "rotation": 0,
+          "target": "5---"
+        },
+        {
+          "yaw": 2.066872286214781,
+          "pitch": -0.47178358889666505,
+          "rotation": 0,
+          "target": "10---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9---------",
+      "name": "Столик на террасе с видом на улицу Счастливых людей",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.7070360461581657,
+        "pitch": 0.22431377620998028,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3210657157937824,
+          "pitch": 0.11685607233390982,
+          "rotation": 0,
+          "target": "10---"
+        },
+        {
+          "yaw": -2.7101901078936077,
+          "pitch": 0.10849719681585768,
+          "rotation": 0,
+          "target": "11------------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10---",
+      "name": "Выход на террасу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3330486796373595,
+          "pitch": 0.21924995418254056,
+          "rotation": 4.71238898038469,
+          "target": "8----"
+        },
+        {
+          "yaw": -0.24732578883553025,
+          "pitch": 0.053557838653651046,
+          "rotation": 0,
+          "target": "9---------"
+        },
+        {
+          "yaw": 1.361996738805761,
+          "pitch": 0.1945044935779059,
+          "rotation": 0,
+          "target": "11------------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11------------",
+      "name": "Столик на террасе с видом на ул. Канделаки и пер. Казем-Бека",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.5948033559804706,
+        "pitch": 0.6469490587759079,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01875742098371269,
+          "pitch": 0.3040305471609024,
+          "rotation": 0,
+          "target": "10---"
+        },
+        {
+          "yaw": 0.9774643050125089,
+          "pitch": 0.09986528981947629,
+          "rotation": 0,
+          "target": "9---------"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
